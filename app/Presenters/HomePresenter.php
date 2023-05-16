84b9fbef355a27a1dbb39aa76aa4d73487a03c23
duffi4545 <71786085+duffi4545@users.noreply.github.com>
2023-05-17 01:41:08 +0200

    ajax

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -7,6 +7,7 @@ namespace App\Presenters;
 use App\Presenters\templates\security\MyAuthenticator;
 use Nette;
 use Nette\Application\UI\Form;
+use Nette\Http\FileUpload;
 
 
 final class HomePresenter extends Nette\Application\UI\Presenter
@@ -26,9 +27,25 @@ final class HomePresenter extends Nette\Application\UI\Presenter
             ->expectsJson()
             ->send();
 
+        $alergen = \Httpful\Request::get('http://localhost:9000/api/v1/ingredients')
+            ->addHeader('Authorization',"Bearer ".$this->user->id)
+            ->send();
+        $classis=[] ;
+        $pocetAlergenu = count($alergen->body);
+        for ($i = 0; $i < $pocetAlergenu / 6; $i++) {
+            $classis[] = "bg-primary";
+            $classis[] = "bg-secondary";
+            $classis[] = "bg-success";
+            $classis[] = "bg-danger";
+            $classis[] = "bg-warning text-dark";
+            $classis[] = "bg-info";
+        }
 
 
         $this->template->foods = $request->body;
+        $this->template->alergens = $alergen->body;
+        $this->template->spanClass = $classis;
+        $this->template->count = 0;
 }
     protected function createComponentLoginForm(): Form
     {
@@ -51,11 +68,20 @@ final class HomePresenter extends Nette\Application\UI\Presenter
 
     protected function createComponentAddFoodForm(): Form
     {
-        $countries = [
-            'CZ' => 'Czech republic',
-            'SK' => 'Slovakia',
-            'GB' => 'United Kingdom',
-        ];
+        $request = \Httpful\Request::get('http://localhost:9000/api/v1/appCategory')
+            ->addHeader('Authorization',"Bearer ".$this->user->id)
+            ->send();
+
+        $categories = [];
+
+        if(!$request->hasErrors()){
+            foreach ($request->body as $category){
+                $categories[$category->id] = $category->name;
+        }
+
+}
+
+
 
         $form = new Form();
         $form->addText('itemName', 'Item Name')
@@ -66,13 +92,14 @@ final class HomePresenter extends Nette\Application\UI\Presenter
             ->setRequired()
             ->addRule(Form::FLOAT, 'Zadejte číslo')
             ->addRule(Form::MIN, 'Price must be greater than 0', 0);
-        $form->addSelect('itemCategory', 'Category ID',$countries)
+        $form->addSelect('itemCategory', 'Category ID',$categories)
             ->setRequired();
         $form->addUpload('itemImage', 'Image File')
             ->setRequired()
             ->addRule(Form::MIME_TYPE, 'Please upload an image', ['image/jpeg', 'image/png']);
-        $form->addText('itemIngredients', 'Ingredients (separated by commas)')
-            ->setRequired();
+        $form->addText('itemIngredients', 'Ingredients (separated by commas)');
+
+        $form->addHidden('itemIngredients_Id', '');
 
         $form->addSubmit('submit', 'Submit');
 
@@ -83,14 +110,62 @@ final class HomePresenter extends Nette\Application\UI\Presenter
 
     public function processForm(Form $form, array $values): void
     {
-        // Zpracování formuláře, např. uložení dat do databáze, nahrání obrázku, atd.
-        // ...
-        // Příklad: Vypište zadané hodnoty formuláře
-        foreach ($values as $name => $value) {
-            dump($name, $value);
+
+
+        if ($form->isSuccess()) {
+
+            /** @var FileUpload $fileUpload */
+            $fileUpload = $values['itemImage'];
+
+            if ($fileUpload->isOk() && $fileUpload->isImage()) {
+
+                if($this->saveFile($fileUpload)){
+
+                    $parser = !empty($values['itemIngredients_Id'])? explode(';', substr_replace($values['itemIngredients_Id'] ,"", -1)) : array();
+
+
+                    $body = [];
+                    $body['name'] = $values['itemName'];
+                    $body['description'] = $values['itemDescription'];
+                    $body['price'] = $values['itemPrice'];
+                    $body['categoryId'] = $values["itemCategory"];
+                    $body['image'] = $fileUpload->name;
+                    $body['ingredients'] = $parser;
+
+
+
+                    $request = \Httpful\Request::post('http://localhost:9000/api/v1/appFood')
+                        ->addHeader('Authorization',"Bearer ".$this->user->id)
+                        ->sendsJson()
+                        ->body(json_encode($body))
+                        ->send();
+
+                    $this->flashMessage($request->body);
+                }else{
+                    $this->flashMessage("soubor se nepodařilo uložit");
+                }
+
+            }
         }
-var_dump($values);
-        die;
+
+        $request = \Httpful\Request::get('http://localhost:9000/api/v1/appFood')
+            ->expectsJson()
+            ->send();
+
+        $this->template->foods = $request->body;
+        $this->redrawControl("foods");
+    }
+
+
+
+    public function saveFile(FileUpload $file): bool
+    {
+        $uploadPath = $this->getUploadPath();
+
+        $fileName = $file->name;
+        $file->move($uploadPath . '/' . $fileName);
+
+        return file_exists($uploadPath."/".$fileName);
     }
 
     public function actionLogin(string $username, string $password): void
@@ -110,6 +185,20 @@ var_dump($values);
         $this->getUser()->logout();
         $this->redirect('home:default');
     }
+    private function getUploadPath(): string
+    {
+        return __DIR__ . '/../../www/uploads';
+    }
 
+    public function handleDelete($id)
+    {
+        $request = \Httpful\Request::delete('http://localhost:9000/api/v1/appFood/'.$id)
+            ->expectsJson()
+            ->send();
+        $this->flashMessage($request->body);
 
+        $this->redrawControl("foods");
+        $this->payload->postGet = true;
+        $this->payload->url = $this->link('this');
+    }
 }
