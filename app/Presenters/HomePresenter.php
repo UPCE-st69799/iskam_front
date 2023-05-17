3fc655dc552a7b5efe730ffe68aedc0265c3ab01
duffi4545 <71786085+duffi4545@users.noreply.github.com>
2023-05-17 20:08:33 +0200

    sorting

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -8,6 +8,7 @@ use App\Presenters\templates\security\MyAuthenticator;
 use Nette;
 use Nette\Application\UI\Form;
 use Nette\Http\FileUpload;
+use function Tester\notNull;
 
 
 final class HomePresenter extends Nette\Application\UI\Presenter
@@ -21,17 +22,15 @@ final class HomePresenter extends Nette\Application\UI\Presenter
     }
 
     public function renderDefault():void{
-
-
-        $request = \Httpful\Request::get('http://localhost:9000/api/v1/appFood')
-            ->expectsJson()
-            ->send();
-
         $alergen = \Httpful\Request::get('http://localhost:9000/api/v1/ingredients')
-            ->addHeader('Authorization',"Bearer ".$this->user->id)
+           // ->addHeader('Authorization',"Bearer ".$this->user->id)
             ->send();
         $classis=[] ;
+        $pocetAlergenu=0;
+
+        if(!$alergen->hasErrors())
         $pocetAlergenu = count($alergen->body);
+
         for ($i = 0; $i < $pocetAlergenu / 6; $i++) {
             $classis[] = "bg-primary";
             $classis[] = "bg-secondary";
@@ -41,77 +40,22 @@ final class HomePresenter extends Nette\Application\UI\Presenter
             $classis[] = "bg-info";
         }
 
-
-        $this->template->foods = $request->body;
-        $this->template->alergens = $alergen->body;
-        $this->template->spanClass = $classis;
-        $this->template->count = 0;
-}
-    protected function createComponentLoginForm(): Form
-    {
-        $form = new Form();
-
-        $form->addText('username', 'Uživatelské jméno:')
-            ->setRequired('Zadejte prosím uživatelské jméno.');
-
-        $form->addPassword('password', 'Heslo:')
-            ->setRequired('Zadejte prosím heslo.');
-
-        $form->addSubmit('login', 'Přihlásit se');
-
-        $form->onSuccess[] = function (Form $form, array $values) {
-            $this->actionLogin($values['username'], $values['password']);
-        };
-
-        return $form;
-    }
-
-    protected function createComponentAddFoodForm(): Form
-    {
-        $request = \Httpful\Request::get('http://localhost:9000/api/v1/appCategory')
-            ->addHeader('Authorization',"Bearer ".$this->user->id)
-            ->send();
-
-        $categories = [];
-
-        if(!$request->hasErrors()){
-            foreach ($request->body as $category){
-                $categories[$category->id] = $category->name;
+        if (!isset($this->template->foods) || !isset($this->template->actualPage) || !isset($this->template->pages)) {
+            $this->readDataWithFilter(null);
         }
 
+        if (!isset($this->template->alergens)) {
+            $this->template->alergens = $alergen->body;
+        }
+
+        if (!isset($this->template->spanClass)) {
+            $this->template->spanClass = $classis;
+        }
 }
 
-
-
-        $form = new Form();
-        $form->addText('itemName', 'Item Name')
-            ->setRequired();
-        $form->addTextArea('itemDescription', 'Item Description')
-            ->setRequired();
-        $form->addText('itemPrice', 'Price')
-            ->setRequired()
-            ->addRule(Form::FLOAT, 'Zadejte číslo')
-            ->addRule(Form::MIN, 'Price must be greater than 0', 0);
-        $form->addSelect('itemCategory', 'Category ID',$categories)
-            ->setRequired();
-        $form->addUpload('itemImage', 'Image File')
-            ->setRequired()
-            ->addRule(Form::MIME_TYPE, 'Please upload an image', ['image/jpeg', 'image/png']);
-        $form->addText('itemIngredients', 'Ingredients (separated by commas)');
-
-        $form->addHidden('itemIngredients_Id', '');
-
-        $form->addSubmit('submit', 'Submit');
-
-        $form->onSuccess[] = [$this, 'processForm'];
-
-        return $form;
-    }
 
     public function processForm(Form $form, array $values): void
     {
-
-
         if ($form->isSuccess()) {
 
             /** @var FileUpload $fileUpload */
@@ -153,7 +97,10 @@ final class HomePresenter extends Nette\Application\UI\Presenter
             ->send();
 
         $this->template->foods = $request->body;
+        $this->template->pages = $request->headers['X-Total-Pages'];
+        $this->template->actualPage = $request->headers['X-Actual-Pages']+1;
         $this->redrawControl("foods");
+        $this->redrawControl("pages");
     }
 
 
@@ -200,5 +147,167 @@ final class HomePresenter extends Nette\Application\UI\Presenter
         $this->redrawControl("foods");
         $this->payload->postGet = true;
         $this->payload->url = $this->link('this');
+    }
+
+
+
+    public function handleFilterFormSubmitted(Form $form, $values)
+    {
+
+        $this->readDataWithFilter($values);
+
+    }
+
+    public function handleRead(int $page){
+        $this->readDataWithFilter(null);
+
+      /*  $this->payload->postGet = true;
+        $this->payload->url = $this->link('this');*/
+    }
+
+
+
+
+
+    public function readDataWithFilter($values):void
+    {
+        if(isset($values)){
+            $parameters = $values;
+        }else{
+            $parameters = $this->getParameters();
+        }
+
+
+        $parser = !empty($parameters['itemIngredientsFilter_Id'])? explode(';', substr_replace($parameters['itemIngredientsFilter_Id'] ,"", -1)) : array();
+
+        $page = 1;
+        if(isset($parameters['page'])){
+            $page = $parameters['page'];
+        }
+        $body = [];
+        if(isset($parameters['name'])||isset($parameters['categoryFilter'])||isset($parameters['itemIngredientsFilter_Id'])||isset($parameters['sort'])){
+
+        $body['name'] = is_null($parameters['name'])?"":$parameters['name'];
+        ($parameters['categoryFilter']!="empty")?$body['categoryId'] = $parameters['categoryFilter']:null;
+        $body['ingredientIdsExclude'] = $parser;
+        $body['order'] = is_null($parameters['sort'])?"":$parameters['sort'];
+        }
+
+
+
+        $request = \Httpful\Request::post('http://localhost:9000/api/v1/appFood/query?size=6&page='.$page-1)
+            ->addHeader('Authorization',"Bearer ".$this->user->id)
+            ->sendsJson()
+            ->body(empty($body)?"{}":json_encode($body))
+            ->send();
+
+
+        $this->template->foods = $request->body;
+        $this->template->pages = $request->headers['X-Total-Pages'];
+        $this->template->actualPage = $page;
+
+        $this->redrawControl('foods');
+        $this->redrawControl("pages");
+
+    }
+
+    protected function createComponentLoginForm(): Form
+    {
+        $form = new Form();
+
+        $form->addText('username', 'Uživatelské jméno:')
+            ->setRequired('Zadejte prosím uživatelské jméno.');
+
+        $form->addPassword('password', 'Heslo:')
+            ->setRequired('Zadejte prosím heslo.');
+
+        $form->addSubmit('login', 'Přihlásit se');
+
+        $form->onSuccess[] = function (Form $form, array $values) {
+            $this->actionLogin($values['username'], $values['password']);
+        };
+
+        return $form;
+    }
+
+    protected function createComponentAddFoodForm(): Form
+    {
+        $request = \Httpful\Request::get('http://localhost:9000/api/v1/appCategory')
+            ->addHeader('Authorization',"Bearer ".$this->user->id)
+            ->send();
+
+        $categories = [];
+
+        if(!$request->hasErrors()){
+            foreach ($request->body as $category){
+                $categories[$category->id] = $category->name;
+            }
+
+        }
+        $form = new Form();
+        $form->addText('itemName', 'Item Name')
+            ->setRequired();
+        $form->addTextArea('itemDescription', 'Item Description')
+            ->setRequired();
+        $form->addText('itemPrice', 'Price')
+            ->setRequired()
+            ->addRule(Form::FLOAT, 'Zadejte číslo')
+            ->addRule(Form::MIN, 'Price must be greater than 0', 0);
+        $form->addSelect('itemCategory', 'Category ID',$categories)
+            ->setRequired();
+        $form->addUpload('itemImage', 'Image File')
+            ->setRequired()
+            ->addRule(Form::MIME_TYPE, 'Please upload an image', ['image/jpeg', 'image/png']);
+        $form->addText('itemIngredients', 'Ingredients (separated by commas)');
+
+        $form->addHidden('itemIngredients_Id', '');
+
+        $form->addSubmit('submit', 'Submit');
+
+        $form->onSuccess[] = [$this, 'processForm'];
+
+        return $form;
+    }
+
+    public function createComponentFilterForm(): Form
+    {
+        $form = new Form();
+
+        $sort = [
+            "" => "",
+            "name" => "name",
+            "price" => "price",
+        ];
+
+        $request = \Httpful\Request::get('http://localhost:9000/api/v1/appCategory')
+            ->addHeader('Authorization', "Bearer " . $this->user->id)
+            ->send();
+
+        $categories = [];
+
+        if (!$request->hasErrors()) {
+            $categories["empty"] = "";
+            foreach ($request->body as $category) {
+                $categories[$category->id] = $category->name;
+            }
+        }
+
+        $form->setMethod('get'); // Nastavení metody na GET
+        $form->setAction(''); // Ponechání prázdné akce pro odeslání na stejnou stránku
+
+        $form->addText('name', 'hledat')
+            ->setHtmlAttribute('placeholder', 'Vyhledat název');
+        $form->addHidden('itemIngredientsFilter_Id');
+        $form->addSelect('categoryFilter', 'Category ID', $categories);
+
+        $form->addSelect('sort', 'Sort', $sort)
+            ->setDefaultValue("");
+
+        $form->addSubmit('filter', 'Filtrovat');
+
+        $form->onSuccess[] = [$this, 'handleFilterFormSubmitted'];
+
+        return $form;
+
     }
 }
